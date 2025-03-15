d07c3e6eac022ef9ef753331acd939dd5cdf66c8
Alberto Rodriguez <arodu.dev@gmail.com>
2025-03-15 10:47:55 -0400

    feat: add Common utility class with methods for copyright year and package version retrieval

--- src/Utility/Common.php
+++ src/Utility/Common.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace UtilityKit\Utility;
@@ -6,37 +7,36 @@ namespace UtilityKit\Utility;
 abstract class Common
 {
     /**
-     * Get the version of the package
-     *
-     * @param string $package
+     * Get current year
+     * 
+     * @param int $startYear
      * @return string
      */
-    public static function getPackageVersion(string $package): ?string
+    public static function getCopyrigthYear(int $startYear): string
     {
-        $composer = json_decode(file_get_contents(ROOT . DS . 'composer.lock'), true);
-        foreach ($composer['packages'] as $item) {
-            if ($item['name'] === $package) {
-                return $item['version'];
+        $currentYear = date('Y');
+        if ($startYear === $currentYear) {
+            return $currentYear;
+        }
+        return $startYear . ' - ' . $currentYear;
+    }
+
+    /**
+     * Get version from composer.lock
+     * 
+     * @param string $pakage
+     * @param bool $cache
+     * @return string|null
+     */
+    public static function packageVersion(string $pakage): ?string
+    {
+        $composerLock = json_decode(file_get_contents(ROOT . DS . 'composer.lock'), true);
+        foreach ($composerLock['packages'] as $package) {
+            if ($package['name'] === $pakage) {
+                return $package['version'];
             }
         }
 
         return null;
-    }
-
-    /**
-     * Get the current year
-     *
-     * @param int|null $year
-     * @return string
-     */
-    public static function getCopyrigthYear(?int $year = null): string
-    {
-        $year = $year ?? date('Y');
-        $currentYear = date('Y');
-        if ($year === $currentYear) {
-            return $year;
-        }
-
-        return $year . ' - ' . $currentYear;
     }
 }
