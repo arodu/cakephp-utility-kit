f305393297e0534b374b98e18fafc16a39282c17
Alberto Rodriguez <arodu.dev@gmail.com>
2025-07-12 11:13:48 -0400

    feat: add RateLimit middleware to manage request limits and handle TooManyRequestsException

---
+++ src/Http/Exception/TooManyRequestsException.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace UtilityKit\Http\Exception;
+
+use Cake\Http\Exception\HttpException;
+use Throwable;
+
+/**
+ * HTTP 429 error.
+ */
+class TooManyRequestsException extends HttpException
+{
+    /**
+     * @inheritDoc
+     */
+    protected $_defaultCode = 429;
+
+    /**
+     * Constructor
+     *
+     * @param string|null $message If no message is given 'TooManyRequests' will be the message
+     * @param int|null $code Status code, defaults to 429
+     * @param \Throwable|null $previous The previous exception.
+     */
+    public function __construct(?string $message = null, ?int $code = null, ?Throwable $previous = null)
+    {
+        if (empty($message)) {
+            $message = 'Too Many Requests';
+        }
+        parent::__construct($message, $code, $previous);
+    }
+}
---
+++ src/Middleware/RateLimitMiddleware.php
@@ -0,0 +1,104 @@
+<?php
+
+declare(strict_types=1);
+
+namespace UtilityKit\Middleware;
+
+use Cake\Cache\Cache;
+use Cake\Core\Configure;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use UtilityKit\Http\Exception\TooManyRequestsException;
+
+/**
+ * RateLimit middleware
+ * 
+ * This middleware limits the number of requests a client can make in a given time period.
+ * It uses the client's IP address to track the number of requests.
+ * If the limit is exceeded, it throws a TooManyRequestsException.
+ * 
+ * Usage:
+ * In your application, you can add this middleware to your middleware stack:
+ * * ```php
+ * * $middlewareQueue->add(new \UtilityKit\Middleware\RateLimitMiddleware([
+ * * *     'limit' => 1000, // Maximum requests allowed
+ * * *     'period' => 3600, // Time period in seconds (default is 1 hour)
+ * * * ]));
+ * 
+ * * Configuration:
+ * * You can also configure the default limit and period in your application configuration:
+ * * * ```php
+ * * * Configure::write('RateLimit', [
+ * * * *     'limit' => 1000, // Default maximum requests allowed
+ * * * *     'period' => 3600, // Default time period in seconds (default is 1 hour)
+ * * * *     'cache' => 'default', // Cache configuration to use
+ * * * ]);
+ */
+class RateLimitMiddleware implements MiddlewareInterface
+{
+
+    /**
+     * Max limit of requests per period.
+     *
+     * @var integer
+     */
+    protected int $limit;
+
+    /**
+     * Time period in seconds for the rate limit.
+     *
+     * @var integer
+     */
+    protected int $period;
+
+    /**
+     * @param array $options
+     */
+    public function __construct(array $options = [])
+    {
+        $config = Configure::read('RateLimit');
+        $this->limit = $options['limit'] ?? $config['limit'] ?? 1000;
+        $this->period = $options['period'] ?? $config['period'] ?? 3600; // Default to 1 hour
+    }
+
+    /**
+     * Process method.
+     *
+     * @param \Psr\Http\Message\ServerRequestInterface $request The request.
+     * @param \Psr\Http\Server\RequestHandlerInterface $handler The request handler.
+     * @return \Psr\Http\Message\ResponseInterface A response.
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        /** @var \Cake\Http\ServerRequest $request */
+        $clientIp = $request->clientIp();
+        $cacheKey = "rate_limit_{$clientIp}";
+
+        $config = Configure::read('RateLimit');
+        $rateData = Cache::read($cacheKey, $config['cache'] ?? 'default');
+
+        if (empty($rateData)) {
+            $rateData = [
+                'count' => 1,
+                'timestamp' => time(),
+            ];
+        } else {
+            if ((time() - $rateData['timestamp']) > $this->period) {
+                $rateData['count'] = 1;
+                $rateData['timestamp'] = time();
+            } else {
+                $rateData['count']++;
+            }
+        }
+
+        Cache::write($cacheKey, $rateData, $config['cache'] ?? 'default');
+
+        if ($rateData['count'] > $this->limit) {
+            throw new TooManyRequestsException();
+        }
+
+        return $handler->handle($request);
+    }
+}
